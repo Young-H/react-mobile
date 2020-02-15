58afa5f0727fb0c89bf5c7f185a0102808ab0ce7
王梓豪 <940144133@qq.com>
2020-02-16 00:43:33 +0800

    最新资讯完成

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Carousel, Flex, Grid } from "antd-mobile";
+import { Carousel, Flex, Grid, WingBlank } from "antd-mobile";
 import axios from "axios";
-import "./index.scss";
+import "./index.scss"
+
 
 // 引入菜单图标
 import Nav1 from "../../assets/imgs/nav-1.png";
@@ -15,14 +16,14 @@ axios.defaults.baseURL = "http://localhost:8080";
 export default class Index extends Component {
   state = {
     swiperData: [],
-    isautoplay: false,
     groupsData: [],
-    newsData:[]
+    newsData:[],
+    isautoplay: false
   };
 
   // 接口拿到轮播图数据
   getSwiperImg = async () => {
-    let { data } = await axios.get("http://localhost:8080/home/swiper");
+    let { data } = await axios.get("/home/swiper");
     if (data.status === 200) {
       this.setState({
         swiperData: data.body
@@ -33,7 +34,6 @@ export default class Index extends Component {
   // 接口拿到租房小组数据
   getGroupData = async () => {
     let { data } = await axios.get("/home/groups");
-    console.log(data);
     if (data.status === 200) {
       this.setState({
         groupsData: data.body
@@ -43,20 +43,22 @@ export default class Index extends Component {
 
   // 接口拿到最新资讯
   getNewsData = async ()=> {
-    const res = await axios.get('/home/news')
-    console.log(res);
-    if (res.status === 200) {
+    const { data } = await axios.get('/home/news')
+    if (data.status === 200) {
       this.setState({
-        newsData:res.body
+        newsData:data.body
       })
     }
     
   }
-  // 组件加载完成，
+
+  // 组件加载完成，填充数据
   componentDidMount() {
     this.getSwiperImg();
     this.getGroupData();
     this.getNewsData()
+    console.log(this.refs.warpper);
+    
   }
 
   // 动态生成轮播图组件
@@ -132,9 +134,35 @@ export default class Index extends Component {
         </div>
     )
   }
+
+  // 动态生成最新资讯组件 
+  renderNews = () => {
+    const newsTag = this.state.newsData.map(item => (
+      <div className='news-item'  key={item.id}>
+        <div className='imgwarp'>
+          <img className='img' src={`http://localhost:8080${item.imgSrc}`} alt=""/>
+        </div>
+        <Flex className='content' justify='between' direction='column'>
+          <h3 className='title'>{item.title}</h3>
+          <Flex className='info' justify='between'>
+            <span>{item.from}</span>
+            <span>{item.date}</span>
+          </Flex>
+        </Flex>
+      </div>
+    ))
+
+    return (
+      <div className="new">
+        <h3 className='news-title'>最新资讯</h3>
+        <WingBlank>{newsTag}</WingBlank>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <div>
+      <div ref='warpper'>
         {/* 顶部轮播图 */}
         {this.swiper()}
         {/* 导航菜单 */}
@@ -142,11 +170,8 @@ export default class Index extends Component {
         {/* 租房小组 */}
         {this.renderGroup()}
         {/* 最新资讯 */}
-        <div className="new">
-          <h3 className='group-title'>最新资讯</h3>
-
-        </div>
+        {this.renderNews()}
       </div>
-    );
+    )
   }
 }
