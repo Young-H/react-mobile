bd61ead33d9b7fdfa7af76455e301b81e23f22a0
王梓豪 <940144133@qq.com>
2020-02-15 23:27:10 +0800

    租房完成

--- src/views/index/index.js
+++ src/views/index/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Carousel,Flex } from "antd-mobile";
+import { Carousel, Flex, Grid } from "antd-mobile";
 import axios from "axios";
-import './index.scss'
+import "./index.scss";
 
 // 引入菜单图标
 import Nav1 from "../../assets/imgs/nav-1.png";
@@ -15,7 +15,9 @@ axios.defaults.baseURL = "http://localhost:8080";
 export default class Index extends Component {
   state = {
     swiperData: [],
-    isload:true
+    isautoplay: false,
+    groupsData: [],
+    newsData:[]
   };
 
   // 接口拿到轮播图数据
@@ -28,14 +30,38 @@ export default class Index extends Component {
     }
   };
 
+  // 接口拿到租房小组数据
+  getGroupData = async () => {
+    let { data } = await axios.get("/home/groups");
+    console.log(data);
+    if (data.status === 200) {
+      this.setState({
+        groupsData: data.body
+      });
+    }
+  };
+
+  // 接口拿到最新资讯
+  getNewsData = async ()=> {
+    const res = await axios.get('/home/news')
+    console.log(res);
+    if (res.status === 200) {
+      this.setState({
+        newsData:res.body
+      })
+    }
+    
+  }
   // 组件加载完成，
   componentDidMount() {
     this.getSwiperImg();
+    this.getGroupData();
+    this.getNewsData()
   }
 
   // 动态生成轮播图组件
   swiper = () => {
-    return this.state.swiperData.map(item => (
+    const swiperTag = this.state.swiperData.map(item => (
       <img
         src={`http://localhost:8080${item.imgSrc}`}
         key={item.id}
@@ -43,44 +69,83 @@ export default class Index extends Component {
         onLoad={() => {
           // 窗口尺寸变化时 -> 触发resize事件 -> 重新设置动态高
           window.dispatchEvent(new Event("resize"));
+          this.setState({
+            isautoplay: true
+          });
         }}
       />
     ));
+    return (
+      <Carousel
+        // 自动切换
+        autoplay={this.state.isautoplay}
+        //  循环播放
+        infinite={true}
+        //  循环播放间隔
+        autoplayInterval="1666"
+      >
+        {swiperTag}
+      </Carousel>
+    );
   };
 
+  // 动态生成菜单组件
+  renderMenu = () => {
+    let menuData = [
+      { id: 1, title: "整租", img: Nav1 },
+      { id: 2, title: "合租", img: Nav2 },
+      { id: 3, title: "地图", img: Nav3 },
+      { id: 4, title: "出租", img: Nav4 }
+    ];
+    const menuTag = menuData.map(item => (
+      <Flex.Item key={item.id}>
+        <img src={item.img} alt="" />
+        <p>{item.title}</p>
+      </Flex.Item>
+    ));
+
+    return <Flex> {menuTag} </Flex>;
+  };
+
+  // 动态生成租房小组组件 
+  renderGroup = ()=> {
+    return (
+      <div className="group">
+          <Flex className="group-title" justify="between">
+            <h3>住房小组</h3>
+            <span>更多</span>
+          </Flex>
+           <Grid
+              data={this.state.groupsData}
+              columnNum={2}
+              square={false}
+              renderItem={data => (
+                <div className='group-item' style={{height:'50px'}}>
+                  <div className='desc'>
+                     <h3>{data.title}</h3>
+                     <p>{data.desc}</p>
+                  </div>
+                  <img src={`http://localhost:8080${data.imgSrc}`} alt=""/>
+                </div>
+              )}
+            />  
+        </div>
+    )
+  }
   render() {
     return (
       <div>
         {/* 顶部轮播图 */}
-        <Carousel
-          // 自动切换
-          autoplay={true}
-          //  循环播放
-          infinite={true}
-          //  循环播放间隔
-          autoplayInterval="1666"
-        >
-          {this.swiper()}
-        </Carousel>
+        {this.swiper()}
         {/* 导航菜单 */}
-        <Flex>
-          <Flex.Item>
-            <img src={Nav1} alt=""/>
-            <p>整租</p>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav2} alt=""/>
-            <p>合租</p>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav3} alt=""/>
-            <p>地图找房</p>
-          </Flex.Item>
-          <Flex.Item>
-            <img src={Nav4} alt=""/>
-            <p>去出租</p>
-          </Flex.Item>
-        </Flex>
+        {this.renderMenu()}
+        {/* 租房小组 */}
+        {this.renderGroup()}
+        {/* 最新资讯 */}
+        <div className="new">
+          <h3 className='group-title'>最新资讯</h3>
+
+        </div>
       </div>
     );
   }
